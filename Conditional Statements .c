8d8bf8335044d4f954e7416eaebb0f792bdf1111
absnimbalkar <65445090+absnimbalkar@users.noreply.github.com>
2021-01-09 19:15:12 +0530

    Create Conditional Statements .c

---
+++ Conditional Statements .c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+void main()
+{
+	int marks;char ch;
+	printf("Enter marks of the student: \n");
+	scanf("%d",&marks);
+	{
+		if(marks>=85 && marks<=100)
+		{
+			printf("Grade A");
+		}
+		else if(marks >=70 && marks<=84)
+		{
+			printf("Grade B");
+		}
+		else if(marks >=55 && marks<=69)
+		{
+			printf("Grade C");
+		}
+		else if(marks >=40 && marks<=54)
+		{
+			printf("Grade D");
+		}
+		else
+		{
+			printf("Grade F");
+		}	
+	}
+}
